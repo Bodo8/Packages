ab4b0eeb052875b30bad73cf99a650a7f9baef12
Bodo8 <b.trojanski@gmial.com>
2022-07-18 21:39:02 +0200

    Dodaj pliki projektów.

---
+++ Packages.Library/Models/ConfigurationModels/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packages.Library.Models.ConfigurationModels
+{
+    public class AppSettings
+    {
+        public int PercentSetToZero { get; set; }
+        public bool RunCheckCheckboxes { get; set; }
+        public bool RunCheckAvailability { get; set; }
+    }
+}
---
+++ Packages.Library/Models/Packet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packages.Library.Models
+{
+    public class Packet
+    {
+        public long UnixTime { get; set; }
+        public bool StateVoltage { get; set; }
+    }
+}
